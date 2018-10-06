b5985d28bb3876087c10781d3e883dc1a6b1ffea
Bogdan Lica <bogdanlica@tutanota.com>
2018-10-06 12:24:57 +0100

    I-exercise_seven

---
+++ exercise_seven.hs
@@ -0,0 +1,7 @@
+-- variable names must start with lowerase letter --> N
+-- use backquote (`) instead of normal quote (')
+-- correct intendation after 'where'
+n = a `div` length xs
+    where
+        a = 10
+        xs = [1,2,3,4,5]
