a1dde862a0aefa82a78e82692e9ef0e1ecaa4872
Bogdan Lica <bogdanlica@tutanota.com>
2018-10-06 12:13:32 +0100

    I-exercise_five

---
+++ exercise_five.hs
@@ -0,0 +1,6 @@
+-- The algorithm does not allow duplicates
+quicksort [] = []
+quicksort (x:xs) = quicksort ls ++ [x] ++ quicksort rs
+                   where 
+                     ls = [ a | a <- xs, a < x ]
+                     rs = [ a | a <- xs, a > x ]
